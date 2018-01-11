8aa74d8f997fff08fc5dfa9934413c14d843c745
Stanley Zhang <stanley@stanleys-mbp.dynamic.ucsd.edu>
2018-01-11 03:51:34 -0800

    help plz

--- index.js
+++ index.js
@@ -86,13 +86,17 @@ function handleMessage(sender_psid, received_message) {
         "text": 'I added ' + arr[0] +' to your shopping list with the maximum price of ' + arr[1]
       }
       var fs = require('fs');
-      console.log(arr[0]+','+arr[1]);
+      fs.open(path, 'w', function(err, fd) {  
+    if (err) {
+        throw 'could not open file: ' + err;
+    }
       fs.appendFile("new_file", arr[0]+'\n'+arr[1]+'\n', 'utf8', function(err) {
         if(err) {
           console.log(err);
         } else {
           console.log("The file was saved!");
         }
+      });
       });
       callSendAPI(sender_psid, response);
     }
