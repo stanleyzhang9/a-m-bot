b4f078b2bfcc92643d1ce94930ea769f69798a8e
Stanley Zhang <stanley@stanleys-mbp.dynamic.ucsd.edu>
2018-01-11 04:14:54 -0800

    help plz god 3

--- index.js
+++ index.js
@@ -87,7 +87,7 @@ function handleMessage(sender_psid, received_message) {
       }
       var fs = require('fs');
       let buffer = arr[0]+'\n'+arr[1]+'\n';
-      fs.writeFile('new_list', buffer, (err) => {if (err) throw err});
+      fs.appendFileSync('new_list', buffer);
       callSendAPI(sender_psid, response);
     }
   }
