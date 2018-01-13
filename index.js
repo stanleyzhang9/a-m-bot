91b4aa289402f766ea390a1e84b323aa6031b064
Stanley Zhang <stanley@stanleys-mbp.dynamic.ucsd.edu>
2018-01-13 00:24:16 -0800

    cleaned up code and README

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const request = require('request');
 
 'use strict';
 
-// Imports dependencies and set up http server
 const
   express = require('express'),
   bodyParser = require('body-parser'),
@@ -16,23 +15,16 @@ app.post('/webhook', (req, res) => {
 
   let body = req.body;
 
-  // Checks this is an event from a page subscription
   if (body.object === 'page') {
 
-    // Iterates over each entry - there may be multiple if batched
     body.entry.forEach(function(entry) {
 
-      // Gets the message. entry.messaging is an array, but
-      // will only ever contain one message, so we get index 0
       let webhook_event = entry.messaging[0];
       console.log(webhook_event);
 
-      // Get the sender PSID
       let sender_psid = webhook_event.sender.id;
       console.log('Sender PSID: ' + sender_psid);
     	
-			// Check if the event is a message or postback and
-  		// pass the event to the appropriate handler function
   		if (webhook_event.message) {
     		handleMessage(sender_psid, webhook_event.message);        
   		} else if (webhook_event.postback) {
@@ -40,48 +32,41 @@ app.post('/webhook', (req, res) => {
   		}
 		});
 
-    // Returns a '200 OK' response to all requests
     res.status(200).send('EVENT_RECEIVED');
   } else {
-    // Returns a '404 Not Found' if event is not from a page subscription
     res.sendStatus(404);
   }
 
 });
 
-// Adds support for GET requests to our webhook
 app.get('/webhook', (req, res) => {
 
-  // Your verify token. Should be a random string.
+  // i'm too lazy to fix this for a small project
   let VERIFY_TOKEN = "<YOUR_VERIFY_TOKEN>"
 
-  // Parse the query params
   let mode = req.query['hub.mode'];
   let token = req.query['hub.verify_token'];
   let challenge = req.query['hub.challenge'];
 
-  // Checks if a token and mode is in the query string of the request
   if (mode && token) {
 
     // Checks the mode and token sent is correct
     if (mode === 'subscribe' && token === VERIFY_TOKEN) {
 
-      // Responds with the challenge token from the request
       console.log('WEBHOOK_VERIFIED');
       res.status(200).send(challenge);
     } else {
-      // Responds with '403 Forbidden' if verify tokens do not match
       res.sendStatus(403);
     }
   }
 });
 
-// Handles messages events
 function handleMessage(sender_psid, received_message) {
   let response;
   if (received_message.text) {
     var arr = received_message.text.split(",");
     if (arr.length == 2) {
+      // write file to be read by python script
       response = {
         "text": 'I added ' + arr[0] +' to your shopping list with the maximum price of ' + arr[1]
       }
@@ -90,7 +75,6 @@ function handleMessage(sender_psid, received_message) {
       fs.writeFile("new_list", buffer, (err) => {
             if (err) throw err;
           });
-  // Construct the message body
   let request_body = {
     "recipient": {
       "id": sender_psid
